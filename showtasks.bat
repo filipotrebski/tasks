b8e8145a72e0cf0f2ffb9af3bdcf224be96e385d
filipotrebski <otrebskifilip@gmail.com>
2019-01-06 15:42:27 +0100

    showtask.bat modified

--- showtasks.bat
+++ showtasks.bat
@@ -1,9 +1,11 @@
-call runcrud goto display
+call runcrud
+if "%ERRORLEVEL%" == "0" goto display
+goto fail
 
 :display
 start chrome http://localhost:8080/crud/v1/task/getTasks
-if "%ERRORLEVEL%" == "0" goto fail
-goto end
+if "%ERRORLEVEL%" == "0" goto end
+goto fail
 
 :fail
 echo.
