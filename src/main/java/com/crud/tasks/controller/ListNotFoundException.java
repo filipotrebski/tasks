0d99b4f941d3e5e7f0910860938ede7e4bf7e202
filo1977 <otrebskifilip@gmail.com>
2019-01-14 23:58:44 +0100

    connection to trello

---
+++ src/main/java/com/crud/tasks/controller/ListNotFoundException.java
@@ -0,0 +1,5 @@
+package com.crud.tasks.controller;
+
+public class ListNotFoundException extends Exception{
+    //todo chyba nie będzie potrzebna
+}
---
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,57 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class TrelloClient {
+
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+
+    @Value("${trello.app.token}")
+    private String trelloAppToken;
+
+    @Value("${trello.app.username}")
+    private String trelloAppUsername;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private URI getTrelloUrl() {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloAppUsername + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloAppToken)
+                .queryParam("fields", "name,id")
+                .build().encode().toUri();
+    }
+
+    public Optional<List<TrelloBoardDto>> getTrelloBoards(){
+
+
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getTrelloUrl(), TrelloBoardDto[].class);
+
+
+//        if (boardsResponse != null) {
+            return Optional.ofNullable((Arrays.asList(boardsResponse)));
+//            .stream()
+//                    .filter(t -> t.getName().toLowerCase().matches(".*kodilla.*"))
+//                    .collect(Collectors.toList());
+//        }
+
+//        return new ArrayList<>();
+    }
+
+}
