f87ad6ea045c001dedebb3d83bbaab1309649e9a
filo1977 <filo1111.fo@gmail.com>
2019-03-13 22:24:15 +0100

    Mail massages configuration

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,21 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
+        context.setVariable("preview_message","Task creation");
         context.setVariable("message", message);
-        context.setVariable("task_url","http://localhost:8888/tasks_frontend/");
-        context.setVariable("button","Visit website");
+        context.setVariable("task_url", "http://localhost:8888/tasks_frontend/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("goodbye_message","Have a nice day");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
